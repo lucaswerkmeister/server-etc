2e92c0063f5e157de1d838788958651190e525c9
Lucas Werkmeister <mail@lucaswerkmeister.de>
2017-10-17 15:58:56 +0200

    Add code.wpia.club git server configuration
    Repositories are served from /srv/git, which is owned by the `git` user.
    (That user isn’t actually used by the below units because they don’t
    need write access to the repositories; it’s only used for pushing to the
    repositories via ssh.)
    
    gitweb.service runs the Git web interface as a FastCGI service, and
    Apache forwards all requests to the code.wpia.club root to that service.
    Other files – static gitweb assets or direct git repository contents for
    dumb HTTP cloning – is served directly by Apache via mod_rewrite. (It
    appears that these mod_rewrite rules trump the Alias in dehydrated.conf,
    so another RewriteRule is added mirroring the Alias.)
    
    git.socket listens on the default port for the git:// protocol and
    starts git-daemon in inetd fashion for every incoming connection.

--- dehydrated/conf.d/50-wellknown.sh
+++ dehydrated/conf.d/50-wellknown.sh
@@ -1,3 +1,4 @@
 WELLKNOWN=/run/dehydrated
 # directory created by systemd through RuntimeDirectory in dehydrated.service
 # and made available by apache2 through Alias in dehydrated.conf
+# and RewriteRule in gitweb.conf
